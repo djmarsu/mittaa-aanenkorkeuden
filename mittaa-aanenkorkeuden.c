27cec981b8a363983eea6a7f2a4f32dca053bb77
nn <nn@nn.n>
2016-05-30 01:44:04 +0300

    viikko 2

--- mittaa-aanenkorkeuden.c
+++ mittaa-aanenkorkeuden.c
@@ -1,23 +1,63 @@
 #include <stdio.h>
 #include <string.h>
 #include <sndfile.h>
+#include <math.h>
 
-#define	BUFLEN 1000
+#define SAMPLERATE 44100
+// 5 guesses in one second
+#define	BUFLEN 8820
 
-static void convert_to_text(SNDFILE *infile) {
+// simple low pass filter algorithm
+float *lpf(float *buf) {
+  static float out[BUFLEN];
+  // initialize array
+  out[0] = buf[0];
+  
+  for (int i = 1; i < BUFLEN; i++) {
+    out[i] = buf[i] + buf[i - 1];
+  }
+
+  return out;
+}
+
+int pitchdetect(float *buf) {
+  // count of zero crossings
+  int count = 0;
+
+  // buffer's length in seconds    
+  float secs = (float) BUFLEN / SAMPLERATE;
+
+  for (int i = 1; i < BUFLEN; i++) {
+    if (buf[i] * buf[i - 1] < 0){
+      count++;
+    }
+  }
+
+  // how many cycles
+  float cyclecount = count / 2;
+  
+  return round(cyclecount / secs);
+}
+
+static void convert_to_text(SNDFILE *file) {
   float buf[BUFLEN];
-  int i, count;
+  int count;
 
-  while ((count = sf_readf_float(infile, buf, BUFLEN)) > 0) {
-    for (i = 0; i < count; i++) {
-      printf("%f\n", buf[i]);
+  // read file to float buffer
+  // TODO: only works with mono files
+  while ((count = sf_readf_float(file, buf, BUFLEN)) > 0) {
+    // make sure there is enough data to detect pitch from
+    if (count == BUFLEN) {
+      // apply simple lowpass filtering to buf
+      float *buf2 = lpf(buf);
+      printf("%d\n", pitchdetect(buf2));
     }
   }
 }
 
 int main (int argc, char **argv) {
-  char *infilename;
-  SNDFILE	*infile = NULL;
+  char *filename;
+  SNDFILE	*file = NULL;
   SF_INFO	sfinfo;
 
   if (argc != 2) {
@@ -25,16 +65,16 @@ int main (int argc, char **argv) {
     return 1;
   }
 
-  infilename = argv[1];
+  filename = argv[1];
   memset(&sfinfo, 0, sizeof(sfinfo));
 
-  if ((infile = sf_open(infilename, SFM_READ, &sfinfo)) == NULL) {
-    printf ("Not able to open input file %s.\n", infilename);
+  if ((file = sf_open(filename, SFM_READ, &sfinfo)) == NULL) {
+    printf("Not able to open input file %s.\n", filename);
     return 1;
   }
 
-  convert_to_text(infile);
-  sf_close(infile);
+  convert_to_text(file);
+  sf_close(file);
 
   return 0;
 }
