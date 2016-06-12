c31a323c3c9f3c1279e8472b9cd1f4ae1cb2fbd7
nn <nn@nn.n>
2016-06-13 01:14:27 +0300

    lisää testejä

--- test/tests.c
+++ test/tests.c
@@ -2,6 +2,7 @@
 
 #include "../config.h"
 #include "../pitchdetect.h"
+#include "../params.h"
 
 #define M_PI 3.14159265358979323846
 
@@ -51,10 +52,40 @@ MU_TEST(test_pitchdetect_with_sine_wave) {
     mu_assert_int_within(1700, v, 10);
 }
 
+MU_TEST(test_count_zerocrossings_works) {
+    float a[buflen] = {0.0};
+    a[0] = -0.31;
+    a[1] = 1.3;
+    a[2] = -0.32;
+    a[3] = 1.11;
+    mu_assert_int_eq(5, count_zerocrossings(a));
+}
+
+MU_TEST(test_sign_works) {
+    mu_assert_int_eq(1, sign(0.1));
+    mu_assert_int_eq(0, sign(0.0));
+    mu_assert_int_eq(-1, sign(-0.3));
+}
+
+MU_TEST(test_ends_with_works) {
+    mu_assert_int_eq(0, ends_with(".wav", "mkrlm.wav"));
+    mu_assert_int_eq(-1, ends_with(".wav", "mekmk.mp3"));
+}
+
+MU_TEST(test_starts_with_works) {
+    mu_assert_int_eq(0, starts_with("hw:", "hw:0,1"));
+    mu_assert_int_eq(-1, starts_with("hw:", "jep"));
+}
+
 MU_TEST_SUITE(test_suite) {
 	MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
   MU_RUN_TEST(test_low_pass_filter);
   MU_RUN_TEST(test_pitchdetect_with_sine_wave);
+  MU_RUN_TEST(test_count_zerocrossings_works);
+  MU_RUN_TEST(test_sign_works);
+  MU_RUN_TEST(test_ends_with_works);
+  MU_RUN_TEST(test_starts_with_works);
+
 }
 
 int main(int argc, char *argv[]) {
